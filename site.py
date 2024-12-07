fc361eeda28e6f48b5b51fc1681d715beedeaeb7
Guilherme O Chagas <guilhermecchagas1999@gmail.com>
2024-12-07 04:25:51 -0300

    adicionando a pasta 'templates' e o primeiro código html

--- site.py
+++ site.py
@@ -1,15 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def homepage():
-    return "Página inicial"
-
-@app.route("/outra_page")
-def outra_page():
-    return "Another page created"
+    return render_template("homepage.html")
 
 if __name__ == "__main__": #Se o código for executado diretamente, irá rodar o 'app.run'... mas se o 'site.py' for importado, o código não será executado.
     app.run(debug=True)
