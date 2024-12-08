6ab5cf4040ad9cd3b2f042d0dd7786178e149a9c
Guilherme O Chagas <guilhermecchagas1999@gmail.com>
2024-12-07 22:00:58 -0300

    alteração no nome de alguns arquivos para padronização / boas práticas

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def homepage():
-    return render_template("homepage.html")
+def index():
+    return render_template("index.html")
 
 if __name__ == "__main__": #Se o código for executado diretamente, irá rodar o 'app.run'... mas se o 'site.py' for importado, o código não será executado.
     app.run(debug=True)
